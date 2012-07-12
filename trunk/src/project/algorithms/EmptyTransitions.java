3b71eecfd3ff48cf6683af56cabb71d28aa9117a
guilhermerx7 <guilhermerx7@809a2ad3-ca5c-364b-8d22-d91edb6c2364>
2012-07-12 18:43:58 +0000

    Revisado algoritmo de eliminação de transição em vazio. Correção de um erro no clone do automato. Atualização dos samples.

--- trunk/src/project/algorithms/EmptyTransitions.java
+++ trunk/src/project/algorithms/EmptyTransitions.java
@@ -3,57 +3,103 @@ package project.algorithms;
 import automata.Automata;
 import automata.State;
 import automata.Transition;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * ETAPA 1 - Algoritmo de Eliminação de Transições em Vazio
  */
 public class EmptyTransitions {
 
-    private Automata original;
+    /**
+     * Mantêm uma cópia do autômato que será modificado
+     */
     private Automata newAutomata;
-    List<Transition> emptyTransitions;
+    /**
+     * Conjunto temporário de transições em vazio
+     */
+    private Set<Transition> emptyTransitions;
 
+    /**
+     * Construtor padrão
+     *
+     * @param automata automato a ser otimizado
+     */
     public EmptyTransitions(Automata automata) {
-        this.original = automata;
-        this.newAutomata = this.original.clone();
-        this.emptyTransitions = new ArrayList<Transition>();
+        this.newAutomata = automata.clone();
+        this.emptyTransitions = new TreeSet<Transition>();
     }
 
+    /**
+     * Executa o algoritmo de eliminação de transições em vazio
+     *
+     * @return autômato equivalente sem as transições em vazio
+     */
     public Automata Start() {
-        List<Transition> empty = getEmpty();
-        do {
+        Set<Transition> empty = getEmptyTransitions();
+        while (empty.size() > 0) {
             for (Transition t : empty) {
-                List<Transition> transitions = getTransitions(t.getState2());
-                if (!t.getState1().equals(t.getState2())) {
+
+                State origin = t.getState1();
+                State destination = t.getState2();
+                // Verifica se o estado de origem é diferente do estado de destino
+                if (!origin.equals(destination)) {
+
+                    // Verifica se o estado de destino é final
+                    if (destination.isFinal()) {
+                        // Adiciona ao estado de origem o atributo final
+                        origin.setType(origin.getType() | State.FINAL);
+                    }
+
+                    // Obtêm todas as transições que partem do estado destino
+                    Set<Transition> transitions = getTransitions(destination);
+
                     for (Transition beingAnalyzed : transitions) {
-                        State newState = getState(t.getState1().getLabel());
-                        newState.setType(newState.getType() | beingAnalyzed.getState1().getType());
-                        this.newAutomata.addTransition(new Transition(newState, beingAnalyzed.getState2(), beingAnalyzed.getSymbol()));
+                        // Adiciona uma nova transição
+                        this.newAutomata.addTransition(new Transition(origin, beingAnalyzed.getState2(), beingAnalyzed.getSymbol()));
                     }
                 }
-                 this.newAutomata.removeTransition(t);
+
+                // Remove a transição que estava sendo analisada
+                this.newAutomata.removeTransition(t);
             }
 
-            empty = getEmpty();
-        } while (empty.size() > 0);
+            empty = getEmptyTransitions();
+        }
+
+        // Remove o símbolo de vazio do alfabeto
+        Iterator<String> iterator = this.newAutomata.getSymbols().iterator();
+        while (iterator.hasNext()) {
+            String symbol = iterator.next();
+            if (symbol.equalsIgnoreCase(Transition.EMPTY_SYMBOL)) {
+                this.newAutomata.getSymbols().remove(symbol);
+                break;
+            }
+        }
 
         return this.newAutomata;
     }
 
-    public boolean isEmpty(Transition transition) {
+    /**
+     * Verifica se a transição consome símbolo vazio
+     *
+     * @param transition transição a ser verificada
+     * @return
+     */
+    private boolean isEmptySymbol(Transition transition) {
         return transition.getSymbol().equalsIgnoreCase(Transition.EMPTY_SYMBOL);
     }
 
-    public void removeEmptyTransition(Transition transition) {
-        this.newAutomata.removeTransition(transition);
-    }
-
-    public List<Transition> getEmpty() {
+    /**
+     * Retorna um conjunto com todas as transições que consomem símbolo vazio do autômato
+     *
+     * @return
+     */
+    private Set<Transition> getEmptyTransitions() {
         emptyTransitions.clear();
         for (Transition originalTransition : this.newAutomata.getTransitions()) {
-            if (isEmpty(originalTransition)) {
+            if (isEmptySymbol(originalTransition)) {
                 emptyTransitions.add(originalTransition);
             }
         }
@@ -61,8 +107,14 @@ public class EmptyTransitions {
         return emptyTransitions;
     }
 
-    public List<Transition> getTransitions(State state) {
-        List<Transition> transitions = new ArrayList<Transition>();
+    /**
+     * Retorna um conjunto com todas as transições que tem origem do estado específicado
+     *
+     * @param state estado de origem
+     * @return
+     */
+    private Set<Transition> getTransitions(State state) {
+        Set<Transition> transitions = new TreeSet<Transition>();
         for (Transition t : this.newAutomata.getTransitions()) {
             if (t.getState1().equals(state)) {
                 transitions.add(t);
@@ -70,9 +122,5 @@ public class EmptyTransitions {
         }
 
         return transitions;
-    }
-
-    public State getState(String label) {
-        return this.newAutomata.getState(label);
     }
 }
